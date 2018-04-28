4297daf1b794bd4894bf6c18bedfb21aa69c965e
duanzhonghui <duanzhonghui@meicai.cn>
2018-04-28 18:46:36 +0800

    使用jobcontrol连接任务1 2 3 4

--- src/com/hadoop/usercf/HadoopUserCfJob.java
+++ src/com/hadoop/usercf/HadoopUserCfJob.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /**
@@ -12,13 +14,16 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  * USER: Administrator
  * DATE: 2018/4/27
  *
- * @描述:
+ * @描述:这个类将所有的任务连接起来成为一个整体
  */
 public class HadoopUserCfJob {
     public static void main(String args[])throws Exception{
+        long start=System.currentTimeMillis();
         String sourcePath="D:/input/data";
         String step1out="D:/output/step1";
         String step3out="D:/output/step3";
+        String step2out="D:/output/step2";
+        String step4out="D:/output/step4";
         //step1设置
         Job step1=Job.getInstance();
         step1.setJarByClass(Step1.class);
@@ -31,7 +36,7 @@ public class HadoopUserCfJob {
         step1.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(step1, new Path(sourcePath));
         FileOutputFormat.setOutputPath(step1, new Path(step1out));
-        step1.waitForCompletion(true);
+       // step1.waitForCompletion(true);
 
         //step3设置
         Job step3=Job.getInstance();
@@ -45,6 +50,52 @@ public class HadoopUserCfJob {
         step3.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(step3, new Path(step1out));
         FileOutputFormat.setOutputPath(step3, new Path(step3out));
-        step3.waitForCompletion(true);
+        //step3.waitForCompletion(true);
+
+        //step2设置
+        Job step2=Job.getInstance();
+        step2.setJarByClass(Step2.class);
+        step2.setJobName("step2");
+        step2.setMapperClass(Step2.Step2Mapper.class);
+        step2.setReducerClass(Step2.Step2Recuder.class);
+        step2.setMapOutputKeyClass(IntWritable.class);
+        step2.setMapOutputValueClass(IntWritable.class);
+        step2.setOutputKeyClass(Text.class);
+        step2.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(step2, new Path(sourcePath));
+        FileOutputFormat.setOutputPath(step2, new Path(step2out));
+
+        //step4设置
+        Job step4=Job.getInstance();
+        step4.setJarByClass(Step4.class);
+        step4.setJobName("step4");
+        step4.setMapperClass(Step4.Step4Mapper.class);
+        step4.setReducerClass(Step4.Step4Reducer.class);
+        step4.setMapOutputKeyClass(Text.class);
+        step4.setMapOutputValueClass(IntWritable.class);
+        step4.setOutputKeyClass(Text.class);
+        step4.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(step4, new Path(step2out));
+        FileOutputFormat.setOutputPath(step4, new Path(step4out));
+
+        ControlledJob controlledJob1=new ControlledJob(step1.getConfiguration());
+        ControlledJob controlledJob2=new ControlledJob(step2.getConfiguration());
+        ControlledJob controlledJob3=new ControlledJob(step3.getConfiguration());
+        ControlledJob controlledJob4=new ControlledJob(step4.getConfiguration());
+        controlledJob3.addDependingJob(controlledJob1);
+        controlledJob4.addDependingJob(controlledJob2);
+        JobControl jobControl=new JobControl("usercfControl");
+        jobControl.addJob(controlledJob1);
+        jobControl.addJob(controlledJob2);
+        jobControl.addJob(controlledJob3);
+        jobControl.addJob(controlledJob4);
+        Thread thread=new Thread(jobControl);
+        thread.start();
+        while (!jobControl.allFinished()){
+            Thread.sleep(1000);
+        }
+        long end=System.currentTimeMillis();
+        System.out.println("计算共花费时间:"+(end-start)/1000+"秒");
+        System.exit(0);
     }
 }
